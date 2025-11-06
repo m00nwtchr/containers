c9e9a780431ddb7ed4399495e7fd9689aae6da3d
m00n <m00nwtchr@duck.com>
2025-11-06 18:44:45 +0100

    ssh-tpm-agent: stable release only

--- apps/ssh-tpm-agent/ci/latest.sh
+++ apps/ssh-tpm-agent/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/Foxboron/ssh-tpm-agent/tags" | jq --raw-output 'first(.[]) | .name' 2>/dev/null)"
+version="$(curl -sX GET "https://api.github.com/repos/Foxboron/ssh-tpm-agent/releases" | jq --raw-output 'first(.[]) | .tag_name' 2>/dev/null)"
 version="${version#*v}"
 printf "%s" "${version}"
